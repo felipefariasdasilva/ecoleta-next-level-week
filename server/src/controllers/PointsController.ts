ce61b5ff37015a40910b53cc2967ff2cea8a23be
felipefariasdasilva <feliipefarias@outlook.com>
2020-06-04 16:05:14 -0300

    created all endpoints

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,42 @@ import { Request, Response} from 'express'
 import knex from '../database/connection'
 
 class PointsController{
+    
+    async index(request: Request, response: Response){
+
+        const { city, uf, items} = request.query
+        
+        const parsedItems = String(items)
+            .split(',')
+            .map( item => Number(item.trim()) )
+
+        const points = await knex('points')
+            .join('points_items', 'points.id', '=', 'point_items.point.id')
+            .whereIn('point_items.item.id', parsedItems)
+            .where('city', String(city))
+            .where('uf', String(uf))
+            .distinct()
+            .select('points.*')
+
+        return response.json(points)
+    }
+
+
+    async show(request: Request, response: Response){
+        const { id } = request.params
+        const point = await knex('points').where('id', id) .first()
+
+        if(!point){
+            return response.status(400).json({ message: 'point not found' })
+        }
+
+        const items = await knex('items')
+            .join('point_items', 'items.id', '=', 'point_items.item.id')
+            .where('point_items.point_id', id)
+            .select('items.title')
+
+        return response.json({ point, items})
+    }
 
     async create(request: Request, response: Response){
 
@@ -19,7 +55,7 @@ class PointsController{
         const trx = await knex.transaction()
         
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
             name,
             email, 
             whatsapp,
@@ -41,6 +77,8 @@ class PointsController{
         })
     
         await trx('point_items').insert(pointItems)
+
+        await trx.commit()
     
         return response.json( { 
             id: point_id,
