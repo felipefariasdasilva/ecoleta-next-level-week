09ea8bcc5a42aca047f32af537ec815733f20d0e
felipefariasdasilva <feliipefarias@outlook.com>
2020-06-22 18:57:56 -0300

    Updates

---
+++ precache-manifest.f5d4f925d434a02d9f1aefa3a97cd0dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a682f70d73f2f20fef7ae764948da438",
+    "url": "/ecoleta-next-level-week/index.html"
+  },
+  {
+    "revision": "1c8b98aaf8ebeabd41e7",
+    "url": "/ecoleta-next-level-week/static/css/main.29b94e20.chunk.css"
+  },
+  {
+    "revision": "ad73f9eca31a5a59cd29",
+    "url": "/ecoleta-next-level-week/static/js/2.48dd8181.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/ecoleta-next-level-week/static/js/2.48dd8181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8b98aaf8ebeabd41e7",
+    "url": "/ecoleta-next-level-week/static/js/main.9ba245c4.chunk.js"
+  },
+  {
+    "revision": "94b354175199180f5f8a",
+    "url": "/ecoleta-next-level-week/static/js/runtime-main.487a1e2a.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/ecoleta-next-level-week/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/ecoleta-next-level-week/static/media/logo.47c4581a.svg"
+  }
+]);
